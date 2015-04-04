3a693bbd9b7ecb7e5e6f18b6970353a4b7be616c
Francesco Shin Bux <francesco.bux@unitn.it>
2015-04-04 12:37:36 +0200

    switched from HEAD to elastic HQ as dashboard plugin

--- layer-custom/recipes/esplugins.rb
+++ layer-custom/recipes/esplugins.rb
@@ -1,11 +1,10 @@
-script "install_plugin_es_head" do
-	interpreter "bash"
-	user "root"
-	cwd "#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/bin/"
-	code <<-EOH
-  	plugin -install mobz/elasticsearch-head
-  	EOH
-	not_if { File.exist?("#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/plugins/head") }
+script "install_plugin_es_hq" do
+    interpreter "bash"
+    user "root"
+    cwd "#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/bin/"
+    code <<-EOH
+    plugin -install royrusso/elasticsearch-HQ
+    EOH
+    not_if { File.exist?("#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/plugins/HQ") }
 end
-
 #notifies :restart, 'service[elasticsearch]' unless node.elasticsearch[:skip_restart]
